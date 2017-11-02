4c73ff405f7fe9335dfe51dd0c752433b77dc74d
stefan jansen <xstefanjansen@gmail.com>
2017-11-02 10:39:49 +0100

    .

--- src/ios/Calls.m
+++ src/ios/Calls.m
@@ -6,25 +6,38 @@
 @implementation Calls
 
 - (void) startListener:(CDVInvokedUrlCommand*)command {
+    // SAVE THE CALLBACKID
     self.callbackId = command.callbackId;
     
+    // START OBSERVER
     CXCallObserver *callObserver = [[CXCallObserver alloc] init];
     [callObserver setDelegate:self queue:nil];
     self.callObserver = callObserver;
     
+    // RETURN A MESSAGES TELLING THE OBSERVER HAS STARTED
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"call listener started"];
     [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
 }
 
 - (void) stopListener:(CDVInvokedUrlCommand*)command {
+
+    // CLEAR THE OBSERVER
+    self.callObserver = nil;
     
+    // CLOSE THE LISTNER CALLBACK
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"call listener stopped"];
     [pluginResult setKeepCallback:[NSNumber numberWithBool:NO]];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
+
+    // CLOSE THIS CALLBACK
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Ok"];
+    [pluginResult setKeepCallback:[NSNumber numberWithBool:NO]];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)callObserver:(CXCallObserver *)callObserver callChanged:(CXCall *)call {
+    // IF THE CALL STATE CHANGED CREATE THE DICTIONARY TO RETURN
     NSDictionary* payload = [[NSDictionary alloc] initWithObjectsAndKeys:
                              [NSNumber numberWithBool:call.outgoing], @"outgoing",
                              [NSNumber numberWithBool:call.hasConnected], @"hasConnected",
@@ -33,6 +46,7 @@
                              nil
                              ];
     
+    // RETURN THE DICTIONARY
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
     [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
     
